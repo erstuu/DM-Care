7c5f1228db52c0956523837c4a200b12962a56dc
Restu Gede Purnama <restumelenoy83@gmail.com>
2025-03-16 10:59:57 +0700

    created MainActivity for navigation host fragment

--- app/src/main/java/com/health/dmcare/activity/MainActivity.kt
+++ app/src/main/java/com/health/dmcare/activity/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.health.dmcare.activity
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.health.dmcare.R
@@ -13,6 +16,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 

a5aa9fb9d1539ca50f600247afe1b66dc4860401
Restu Gede Purnama <restumelenoy83@gmail.com>
2025-03-16 10:57:29 +0700

    create custom splash screen

--- app/src/main/java/com/health/dmcare/activity/SplashScreenActivity.kt
+++ app/src/main/java/com/health/dmcare/activity/SplashScreenActivity.kt
@@ -1,21 +1,53 @@
 package com.health.dmcare.activity
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.health.dmcare.R
+import com.health.dmcare.databinding.ActivitySplashScreenBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
+@SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : AppCompatActivity() {
+    private var _binding: ActivitySplashScreenBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_splash_screen)
+
+        _binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        installSplashScreen()
+    }
+
+    private fun installSplashScreen() {
+        Handler(Looper.getMainLooper()).postDelayed({
+            val toOnboardingActivity = Intent(this, OnboardingActivity::class.java)
+            startActivity(toOnboardingActivity)
+            finish()
+        }, 2000)
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }

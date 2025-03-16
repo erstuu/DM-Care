331039dfb30da753663cf041f2ff459ac5cf5fb0
Restu Gede Purnama <restumelenoy83@gmail.com>
2025-03-16 10:58:07 +0700

    add onboarding page

--- app/src/main/java/com/health/dmcare/activity/OnboardingActivity.kt
+++ app/src/main/java/com/health/dmcare/activity/OnboardingActivity.kt
@@ -1,21 +1,62 @@
 package com.health.dmcare.activity
 
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.shape.RoundedCornerTreatment
+import com.google.android.material.shape.ShapeAppearanceModel
 import com.health.dmcare.R
+import com.health.dmcare.databinding.ActivityOnboardingBinding
+import com.google.android.material.shape.MaterialShapeDrawable
 
 class OnboardingActivity : AppCompatActivity() {
+    private var _binding: ActivityOnboardingBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        _binding = ActivityOnboardingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_onboarding)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.buttonOnboarding.setOnClickListener {
+            val toMainActivity = Intent(this, MainActivity::class.java)
+            startActivity(toMainActivity)
+            finish()
+        }
+
+        setupCardShape()
+    }
+
+    private fun setupCardShape() {
+        val cornerSizeMessage = resources.getDimension(R.dimen.corner_radius)
+
+        val shapeAppearanceModel = ShapeAppearanceModel.builder()
+            .setTopRightCorner(RoundedCornerTreatment())
+            .setTopRightCornerSize(cornerSizeMessage)
+            .setTopLeftCorner(RoundedCornerTreatment())
+            .setTopLeftCornerSize(cornerSizeMessage)
+            .build()
+
+        binding.materialCardViewOnboarding.shapeAppearanceModel = shapeAppearanceModel
+        binding.materialCardViewOnboarding.strokeWidth = 0
+        binding.materialCardViewOnboarding.elevation = 4f
+        binding.materialCardViewOnboarding.setCardBackgroundColor(getColor(R.color.white))
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }

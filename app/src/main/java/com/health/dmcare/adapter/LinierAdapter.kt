f37440e60eb27266cc90f157f3b720d451522595
Restu Gede Purnama <restumelenoy83@gmail.com>
2025-03-16 22:58:10 +0700

    add new adapter for linier orientation

---
+++ app/src/main/java/com/health/dmcare/adapter/LinierAdapter.kt
@@ -0,0 +1,31 @@
+package com.health.dmcare.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.health.dmcare.databinding.ItemRvTandaDanGejalaBinding
+import com.health.dmcare.models.TandaGejalaDiabetes
+
+class LinierAdapter(private val items: List<TandaGejalaDiabetes>)
+    : RecyclerView.Adapter<LinierAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = ItemRvTandaDanGejalaBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    class ViewHolder(private val binding: ItemRvTandaDanGejalaBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: TandaGejalaDiabetes) {
+            binding.ivTandaDanGejala.setImageResource(item.image)
+            binding.tvTitTandaDanGejala.text = item.title
+            binding.tvSubTandaDanGejala.text = item.description?: ""
+        }
+    }
+}

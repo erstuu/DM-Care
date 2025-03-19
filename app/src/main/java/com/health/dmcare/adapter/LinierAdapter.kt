ea3cd04889b044b58b1b8ec6ac8bd3696f3a6689
Restu Gede Purnama <restumelenoy83@gmail.com>
2025-03-19 19:36:31 +0700

    add new library versions for CircleImageView and core YouTube player

--- app/src/main/java/com/health/dmcare/adapter/LinierAdapter.kt
+++ app/src/main/java/com/health/dmcare/adapter/LinierAdapter.kt
@@ -4,9 +4,9 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.health.dmcare.databinding.ItemRvTandaDanGejalaBinding
-import com.health.dmcare.models.TandaGejalaDiabetes
+import com.health.dmcare.models.DataCardDiabetes
 
-class LinierAdapter(private val items: List<TandaGejalaDiabetes>)
+class LinierAdapter(private val items: List<DataCardDiabetes>)
     : RecyclerView.Adapter<LinierAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -22,7 +22,7 @@ class LinierAdapter(private val items: List<TandaGejalaDiabetes>)
 
     class ViewHolder(private val binding: ItemRvTandaDanGejalaBinding) : RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(item: TandaGejalaDiabetes) {
+        fun bind(item: DataCardDiabetes) {
             binding.ivTandaDanGejala.setImageResource(item.image)
             binding.tvTitTandaDanGejala.text = item.title
             binding.tvSubTandaDanGejala.text = item.description?: ""

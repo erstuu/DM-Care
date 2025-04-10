2003cbfbe216a205224a155bc50d145649971199
Restu Gede Purnama <restumelenoy83@gmail.com>
2025-04-10 14:33:22 +0700

    add AboutFragment and its layout for application information

---
+++ app/src/main/java/com/health/dmcare/fragment/AboutFragment.kt
@@ -0,0 +1,66 @@
+package com.health.dmcare.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.core.content.ContextCompat
+import androidx.navigation.fragment.findNavController
+import com.health.dmcare.R
+import com.health.dmcare.databinding.FragmentAboutBinding
+import com.health.dmcare.util.TextFormatterHelper
+
+class AboutFragment : Fragment() {
+    private var _binding: FragmentAboutBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentAboutBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        statusBarSetup()
+        setupToolbar()
+        formatText()
+    }
+
+    private fun formatText() {
+        TextFormatterHelper.makeNumberedList(
+            requireContext(),
+            binding.listPengembang,
+            R.array.list_pengembang_array
+        )
+    }
+
+    @Suppress("DEPRECATION")
+    private fun statusBarSetup() {
+        requireActivity().window.apply {
+            addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+            statusBarColor = ContextCompat.getColor(requireContext(), R.color.blue)
+            decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        }
+    }
+
+    private fun setupToolbar() {
+        val toolbar = binding.toolbar
+        toolbar.isTitleCentered = true
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_ios_new_24)
+        toolbar.setNavigationOnClickListener {
+            findNavController().navigateUp()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
